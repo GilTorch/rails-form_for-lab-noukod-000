06c288268be0de29c587953592b47f5fe86d0aee
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 21:53:06 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,29 @@
+class SchoolClassesController < ApplicationController
+  def new
+    @school_class=SchoolClass.new
+  end
+
+  def show
+    @school_class=SchoolClass.find(params[:id])
+  end
+
+  def edit
+    @school_class=SchoolClass.find(params[:id])
+  end
+
+  def create
+    @school_class=SchoolClass.create(school_classes_params(:title,:description))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def update
+    @school_class=SchoolClass.find(params[:id])
+    @school_class.update(school_classes_params(:title,:description))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def school_classes_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
+end
